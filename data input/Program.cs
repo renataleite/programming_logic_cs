4d6133c280e14ee0ee89f817d1f9ac791a772b40
Renata Guedes <renataleitesg@gmail.com>
2020-09-14 15:19:17 +0100

    18. Into data in C#.

--- data input/Program.cs
+++ data input/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace data_input
 {
@@ -6,24 +7,41 @@ namespace data_input
     {
         static void Main(string[] args)
         {
-            string frase = Console.ReadLine();
-            string x = Console.ReadLine();
-            string y = Console.ReadLine();
-            string z = Console.ReadLine();
-           
-            string[] v = Console.ReadLine().Split(" ");
-            string a = v[0];
-            string b = v[1];
-            string c = v[2];
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("How many rooms do you have in your house?");
+            int bedrooms = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the product price:");
+            double price = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your name, age and height (same line):");
+            string[] profile = Console.ReadLine().Split();
+            string nameProfile = profile[0];
+            int ageProfile = int.Parse(profile[1]);
+            double heightProfile = double.Parse(profile[2]);
+            Console.WriteLine(name);
+            Console.WriteLine(bedrooms);
+            Console.WriteLine(price);
+            Console.WriteLine(nameProfile);
+            Console.WriteLine(ageProfile);
+            Console.WriteLine(heightProfile);
 
-            Console.WriteLine("Você digitou: " );
-            Console.WriteLine(frase);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
+            /*int intNumber = int.Parse(Console.ReadLine());
+            char caracter = char.Parse(Console.ReadLine());
+            double doubleNumber = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            string[] NameSexAgeHeight = Console.ReadLine().Split(' ');
+            string name = NameSexAgeHeight[0];
+            char sex = char.Parse(NameSexAgeHeight[1]);
+            int age = int.Parse(NameSexAgeHeight[2]);
+            double height = double.Parse(NameSexAgeHeight[3]);
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(intNumber);
+            Console.WriteLine(caracter);
+            Console.WriteLine(doubleNumber);
+            Console.WriteLine(name);
+            Console.WriteLine(sex);
+            Console.WriteLine(age);
+            Console.WriteLine(height);*/
         }
     }
 }
