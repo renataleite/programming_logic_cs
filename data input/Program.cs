7079a5cd567115b22ee04f1b8dc069bfe9506e3e
Renata Guedes <renataleitesg@gmail.com>
2020-09-18 11:37:01 +0100

    27. Exercise solved

--- data input/Program.cs
+++ data input/Program.cs
@@ -6,23 +6,13 @@ namespace data_input
     {
         static void Main(string[] args)
         {
-            string[] values = Console.ReadLine().Split(" ");
-            double A = double.Parse(values[0]);
-            double B = double.Parse(values[1]);
-            double C = double.Parse(values[2]);
-            double pi = 3.14159;
+            int seconds = int.Parse(Console.ReadLine());
+            int hours = seconds / 3600;
+            int minutes = (seconds % 3600) / 60;
+            seconds = (seconds % 3600) % 60;
 
-            double TRIANGLE = (A * C)/2;
-            double CIRCLE = pi * Math.Pow(C, 2);
-            double TRAPEZIUM = C / 2 * (A + B);
-            double SQUARE = B * B;
-            double RECTANGLE = A * B;
+            Console.WriteLine(hours + ":" + minutes + ":"+ seconds);
 
-            Console.WriteLine("TRIANGLE: " + TRIANGLE.ToString("F3"));
-            Console.WriteLine("CIRCLE: " + CIRCLE.ToString("F3"));
-            Console.WriteLine("TRAPEZIUM: " + TRAPEZIUM.ToString("F3"));
-            Console.WriteLine("SQUARE: " + SQUARE.ToString("F3"));
-            Console.WriteLine("RECTANGLE: " + RECTANGLE.ToString("F3"));
         }
     }
 }
